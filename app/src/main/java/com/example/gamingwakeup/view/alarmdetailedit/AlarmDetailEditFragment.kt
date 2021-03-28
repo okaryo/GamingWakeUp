00211b813407a02e17a4937daa55b7acbaf4ada8
okaryo <okaryotr@gmail.com>
2021-03-28 20:30:47 +0900

    [add] fragment and viewmodel for detailedit page

---
+++ app/src/main/java/com/example/gamingwakeup/view/alarmdetailedit/AlarmDetailEditFragment.kt
@@ -0,0 +1,29 @@
+package com.example.gamingwakeup.view.alarmdetailedit
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.gamingwakeup.databinding.FragmentAlarmDetailEditBinding
+import com.example.gamingwakeup.viewmodel.alarmdetailedit.AlarmDetailEditViewModel
+import com.example.gamingwakeup.viewmodel.alarmdetailedit.AlarmDetailEditViewModelFactory
+
+class AlarmDetailEditFragment : Fragment() {
+    private lateinit var viewModel: AlarmDetailEditViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = FragmentAlarmDetailEditBinding.inflate(layoutInflater)
+        val viewModelFactory = AlarmDetailEditViewModelFactory()
+        viewModel =
+            ViewModelProvider(this, viewModelFactory).get(AlarmDetailEditViewModel::class.java)
+        binding.viewmodel = viewModel
+
+        return binding.root
+    }
+}
