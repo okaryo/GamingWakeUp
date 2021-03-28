3cb5d7e2907a2fca4b01f0806f49dce94e835f4b
okaryo <okaryotr@gmail.com>
2021-03-28 20:31:07 +0900

    [modify] alarmlist to navigate to detailedit page

--- app/src/main/java/com/example/gamingwakeup/view/alarmlist/AlarmListFragment.kt
+++ app/src/main/java/com/example/gamingwakeup/view/alarmlist/AlarmListFragment.kt
@@ -6,9 +6,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.RecyclerView
 import com.example.gamingwakeup.R
 import com.example.gamingwakeup.databinding.FragmentAlarmListBinding
+import com.example.gamingwakeup.model.Alarm
 import com.example.gamingwakeup.viewmodel.alarmlist.AlarmListViewModel
 import com.example.gamingwakeup.viewmodel.alarmlist.AlarmListViewModelFactory
 
@@ -25,8 +30,24 @@ class AlarmListFragment : Fragment() {
         viewModel = ViewModelProvider(this, viewModelFactory).get(AlarmListViewModel::class.java)
         binding.lifecycleOwner = this
         binding.viewmodel = viewModel
-        binding.fragmentAlarmList.adapter = AlarmListAdapter()
+        setAdapterToRecyclerView(binding.fragmentAlarmList)
+        setNavigationObserver(viewModel.navigateToAlarmDetailEdit)
 
         return binding.root
+    }
+
+    private fun setAdapterToRecyclerView(recyclerView: RecyclerView) {
+        recyclerView.adapter = AlarmListAdapter(AlarmListAdapter.OnClickListener {
+            viewModel.navigateToAlarmDetailEdit(it)
+        })
+    }
+
+    private fun setNavigationObserver(navigation: LiveData<Alarm>) {
+        navigation.observe(this, Observer {
+            if (it != null) {
+                this.findNavController()
+                    .navigate(AlarmListFragmentDirections.actionAlarmListFragmentToAlarmDetailEditFragment())
+            }
+        })
     }
 }
