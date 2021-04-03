8dbed3d1d2bf72231a73fb53e99b1efc9423d1fa
okaryo <okaryotr@gmail.com>
2021-04-03 21:14:38 +0900

    [fix] bindingadapter to handle with null

--- app/src/main/java/com/example/gamingwakeup/view/BindingAdapters.kt
+++ app/src/main/java/com/example/gamingwakeup/view/BindingAdapters.kt
@@ -6,7 +6,9 @@ import com.example.gamingwakeup.model.AlarmList
 import com.example.gamingwakeup.view.alarmlist.AlarmListAdapter
 
 @BindingAdapter("listData")
-fun bindRecyclerView(recyclerView: RecyclerView, data: AlarmList) {
+fun bindRecyclerView(recyclerView: RecyclerView, data: AlarmList?) {
+    if (data == null) return
+
     val adapter = recyclerView.adapter as AlarmListAdapter
     adapter.submitList(data.values)
 }
