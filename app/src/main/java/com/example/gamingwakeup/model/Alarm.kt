4703ce5956c1ba8a4e373202d9dbbc0811b7e762
okaryo <okaryotr@gmail.com>
2021-04-03 21:10:55 +0900

    [add] Alarm entity for dao

--- app/src/main/java/com/example/gamingwakeup/model/Alarm.kt
+++ app/src/main/java/com/example/gamingwakeup/model/Alarm.kt
@@ -1,9 +1,34 @@
 package com.example.gamingwakeup.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "alarms")
 data class Alarm(
+    @PrimaryKey(autoGenerate = true) val id: Int,
     val hour: Int,
     val minute: Int,
-    val soundVolume: Int,
-    val hasVibration: Boolean = true,
-    val isOn: Boolean = false
-)
+    @ColumnInfo(name = "sound_volume") val soundVolume: Int,
+    @ColumnInfo(name = "has_vibration") val hasVibration: Boolean = true,
+    @ColumnInfo(name = "is_turned_on") val isTurnedOn: Boolean = false
+) {
+    companion object {
+        fun createForInsert(
+            hour: Int,
+            minute: Int,
+            soundVolume: Int,
+            hasVibration: Boolean,
+            isTurnedOn: Boolean
+        ): Alarm {
+            return Alarm(
+                id = 0,
+                hour = hour,
+                minute = minute,
+                soundVolume = soundVolume,
+                hasVibration = hasVibration,
+                isTurnedOn = isTurnedOn
+            )
+        }
+    }
+}
