3b5f90f0d77cff919f76fe03d3f77d8e2d610dad
okaryo <okaryotr@gmail.com>
2021-04-06 21:34:43 +0900

    [modify] AddEditAlarmViewModel to add two-way databinding

--- app/src/main/java/com/example/gamingwakeup/viewmodel/AddEditAlarmViewModel.kt
+++ app/src/main/java/com/example/gamingwakeup/viewmodel/AddEditAlarmViewModel.kt
@@ -1,82 +1,88 @@
 package com.example.gamingwakeup.viewmodel
 
 import android.app.Application
+import androidx.databinding.BaseObservable
+import androidx.databinding.Bindable
 import androidx.lifecycle.*
 import com.example.gamingwakeup.model.Alarm
 import com.example.gamingwakeup.repository.AlarmRepository
 import com.example.gamingwakeup.room.database.AlarmDatabase
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
-import java.lang.IllegalArgumentException
+import kotlinx.coroutines.runBlocking
+import java.util.*
 
-class AddEditAlarmViewModel(private val repository: AlarmRepository) : ViewModel() {
-    private val _alarmId = MutableLiveData<Int>()
-    private val _hour = MutableLiveData<Int>()
-    private val _minute = MutableLiveData<Int>()
-    private val _soundVolume = MutableLiveData<Int>()
+class AddEditAlarmViewModel(private val repository: AlarmRepository, calender: Calendar) : BaseObservable() {
+    @Bindable var soundVolume: Int = 50
+    @Bindable var hour = calender.get(Calendar.HOUR_OF_DAY)
+    @Bindable var minute = calender.get(Calendar.MINUTE)
+    private var _alarmId = 0
     private val _hasVibration = MutableLiveData(true)
     private var isNewAlarm = false
 
-//    val alarmId: LiveData<String?>
-//        get() = _alarmId
-//    val hour: LiveData<Int>
-//        get() = _hour
-//    val minute: LiveData<Int>
-//        get() = _minute
-//    val soundVolume: LiveData<Int>
-//        get() = _soundVolume
-//    val hasVibration: LiveData<Boolean>
-//        get() = _hasVibration
+    val hasVibration: LiveData<Boolean>
+        get() = _hasVibration
 
     fun initialize(alarmId: Int) {
-        _alarmId.value = alarmId
-        if (_alarmId.value == 0) {
+        _alarmId = alarmId
+        if (_alarmId == 0) {
             isNewAlarm = true
+        } else {
+            val alarm = runBlocking {
+                repository.getAlarm(_alarmId)
+            }
+            hour = alarm.hour
+            minute = alarm.minute
+            soundVolume = alarm.soundVolume
+            _hasVibration.value = alarm.hasVibration
         }
-        _hour.value = 10
-        _minute.value = 10
-        _soundVolume.value = 10
-        _hasVibration.value = true
+    }
 
+    fun onVibrationValueChanged(isChecked: Boolean) {
+        _hasVibration.value = isChecked
     }
 
     fun saveAlarm() {
-        val currentHour = _hour.value
-        val currentMinute = _minute.value
-        val currentSoundVolume = _soundVolume.value
-        val currentHasVibration = _hasVibration.value
-        if (currentHour == null || currentMinute == null || currentSoundVolume == null || currentHasVibration == null) {
-            return
-        }
+        val currentHasVibration = _hasVibration.value ?: return
         if (isNewAlarm) {
             val alarm = Alarm.createForInsert(
-                hour = currentHour,
-                minute = currentMinute,
-                soundVolume = currentSoundVolume,
+                hour = hour,
+                minute = minute,
+                soundVolume = soundVolume,
                 hasVibration = currentHasVibration,
                 isTurnedOn = true
             )
             createAlarm(alarm)
+        } else {
+            val alarm = Alarm(
+                id = _alarmId,
+                hour = hour,
+                minute = minute,
+                soundVolume = soundVolume,
+                hasVibration = currentHasVibration,
+                isTurnedOn = true
+            )
+            updateAlarm(alarm)
         }
     }
 
     suspend fun deleteAlarm() {
-        val alarmId = _alarmId.value
-        if (alarmId != null && !isNewAlarm) {
-            val alarm = repository.getAlarm(alarmId)
+        if (!isNewAlarm) {
+            val alarm = repository.getAlarm(_alarmId)
             repository.deleteAlarm(alarm)
         }
     }
 
-    private fun createAlarm(alarm: Alarm) = viewModelScope.launch { repository.createAlarm(alarm) }
+    private fun createAlarm(alarm: Alarm) = GlobalScope.launch { repository.createAlarm(alarm) }
 
-    class Factory(private val application: Application) : ViewModelProvider.Factory {
-        override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-            if (modelClass.isAssignableFrom(AddEditAlarmViewModel::class.java)) {
-                val database = AlarmDatabase.getInstance(application.applicationContext)
-                val repository = AlarmRepository(database)
-                return AddEditAlarmViewModel(repository) as T
-            }
-            throw IllegalArgumentException("Unknown ViewModel class!")
+    private fun updateAlarm(alarm: Alarm) = GlobalScope.launch { repository.updateAlarm(alarm) }
+
+    companion object {
+        fun create(application: Application): AddEditAlarmViewModel {
+            val calender = Calendar.getInstance(TimeZone.getTimeZone("Asia/Tokyo"), Locale.JAPAN)
+            val database = AlarmDatabase.getInstance(application.applicationContext)
+            val repository = AlarmRepository(database)
+            return AddEditAlarmViewModel(repository, calender)
         }
     }
 }
