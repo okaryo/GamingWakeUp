8f943eba80b057967aa97a802fb8cd81072bae5f
okaryo <okaryotr@gmail.com>
2021-04-25 17:22:52 +0900

    [add] new property to Alarm model

---
+++ app/src/main/java/com/example/gamingwakeup/model/model/VibrationSetting.kt
@@ -0,0 +1,5 @@
+package com.example.gamingwakeup.model.model
+
+data class VibrationSetting(
+    val active: Boolean
+)
---
+++ app/src/main/java/com/example/gamingwakeup/model/model/WeeklyRecurringSetting.kt
@@ -0,0 +1,11 @@
+package com.example.gamingwakeup.model.model
+
+data class WeeklyRecurringSetting(
+    val monday: Boolean,
+    val tuesday: Boolean,
+    val wednesday: Boolean,
+    val thursday: Boolean,
+    val friday: Boolean,
+    val saturday: Boolean,
+    val sunday: Boolean
+)
