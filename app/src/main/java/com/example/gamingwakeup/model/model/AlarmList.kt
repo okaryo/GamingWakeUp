de7a5f688a3d9b001b15fafcdb87c4aa62441369
okaryo <okaryotr@gmail.com>
2021-05-08 01:38:22 +0900

    [add] sort method to AlarmList

--- app/src/main/java/com/example/gamingwakeup/model/model/AlarmList.kt
+++ app/src/main/java/com/example/gamingwakeup/model/model/AlarmList.kt
@@ -1,3 +1,12 @@
 package com.example.gamingwakeup.model.model
 
-data class AlarmList(val values: List<Alarm>)
+data class AlarmList(
+    val values: List<Alarm>
+) {
+    fun sort(): AlarmList {
+        val sortedValues = values.toMutableList().apply {
+            sortBy { it.hour * 60 + it.minute }
+        }.toList()
+        return AlarmList(values = sortedValues)
+    }
+}
